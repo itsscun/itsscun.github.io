338b103ece47eeda480dfd74b1e029aaeae33748
SCÜN <remi.georges20@gmail.com>
2024-04-10 16:23:23 +0200

    btn scoll top

--- script.js
+++ script.js
@@ -15,3 +15,28 @@ document.addEventListener('DOMContentLoaded', function() {
         download_file(pdfUrl, pdfFileName);
     });
 });
+
+let calcScrollValue = () => {
+    let scrollProgress = document.getElementById("progress");
+    let progressValue = document.getElementById("progress-value");
+    let pos = document.documentElement.scrollTop;
+    let calcHeight = 
+    document.documentElement.scrollHeight - 
+    document.documentElement.clientHeight;
+    
+    let scrollValue = Math.round((pos * 100) / calcHeight);
+
+    if(pos > 100) {
+        scrollProgress.style.display = "grid";
+    }
+    else{
+        scrollProgress.style.display = "none";
+    }
+    scrollProgress.addEventListener("click", () => {
+        document.documentElement.scrollTop = 0;
+    });
+    scrollProgress.style.background = 'conic-gradient(#03cc65 ${scrollValue}%, #D7D7D7 ${scrollValue}%)';
+};
+
+window.onscroll = calcScrollValue;
+window.onload = calcScrollValue;
